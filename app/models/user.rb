0b70601f3e8c649b4489c4b79044e25317b9a7a1
aarif2000 <arif@webkorps.com>
2022-09-23 21:48:32 +0530

    added omniauth fields to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,16 +8,22 @@ class User < ApplicationRecord
 
 
          def self.from_omniauth(access_token)
-          data = access_token.info
-          user = User.where(email: data['email']).first
+          # data = access_token.info
+          user = User.where(email: access_token.info.email).first
       
           unless user
               user = User.create(
-                 email: data['email'],
+                 email: access_token.info.email,
                  password: Devise.friendly_token[0,20]
               )
           end
+          user.provider =access_token.provider 
+          user.uid =access_token.uid 
+          user.name =access_token.info.name
+          user.image =access_token.info.image 
+          user.save 
           user
+          
       end
       
 end
