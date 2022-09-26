85cc2078e2c9db16ed05c111cafcc31fc57ff338
aarif2000 <arif@webkorps.com>
2022-09-26 20:09:16 +0530

    added users after sign in path

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   root 'static_public#landing_page'
   get 'privacy', to: 'static_public#privacy'
   get 'terms', to: 'static_public#terms'
-  resources :users, only: %i[index]
+  resources :users, only: %i[index show] 
 
 end
