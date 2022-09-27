472c0d09f2ff1bdbbfd1e198a47f9c258a58e5d9
aarif2000 <arif@webkorps.com>
2022-09-28 02:58:54 +0530

    added pagy

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -1,5 +1,5 @@
-Pagy::VARS[:items] = 10
-Pagy::VARS[:cycle] = true
-Pagy::VARS[:overflow] = :last_page
 require 'pagy/extras/bootstrap'
 require 'pagy/extras/overflow'
+# Pagy::VARS[:items] = 10
+# Pagy::VARS[:cycle] = true
+# Pagy::VARS[:overflow] = :last_page
