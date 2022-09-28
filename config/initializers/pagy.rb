cfd53ddf7d4ef286a4d485d4d2bb47d4975bb317
aarif2000 <arif@webkorps.com>
2022-09-28 15:15:14 +0530

    first commit

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -1,5 +1,3 @@
 require 'pagy/extras/bootstrap'
 require 'pagy/extras/overflow'
-# Pagy::VARS[:items] = 10
-# Pagy::VARS[:cycle] = true
-# Pagy::VARS[:overflow] = :last_page
+Pagy::DEFAULT[:items] = 10
